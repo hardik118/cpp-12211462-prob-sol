3ea9e5bf0d347a7b3ba01012f73c16db1c8d655c
hardik yadav <hardikyadav1182005@gmail.com>
2023-08-10 23:02:53 +0530

    updated

---
+++ cpp/right handside star pattern in cpp.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+ 
+// Driver Code
+int main()
+{
+    int n = 5;
+ 
+    // looping rows
+    for (int i = n; i > 0; i--) {
+        for (int j = 1; j <= n; j++) // looping columns
+        {
+            if (j >= i) {
+                cout << "* ";
+            }
+            else {
+                cout << "  ";
+            }
+        }
+        cout << endl;
+    }
+    return 0;
+}
