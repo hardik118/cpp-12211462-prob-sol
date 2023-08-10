3e184a87d6c833b02fe8c02a85a3274d224f29d1
hardik yadav <hardikyadav1182005@gmail.com>
2023-08-10 22:55:54 +0530

    new

---
+++ cpp/star pattern in cpp.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int i, space, j;
+    for(i=1; i<=6; i++)
+    {
+        for(space=1; space<i; space++)
+            cout<<" ";
+        for(j=i; j<=6; j++)
+        {
+            cout<<"* ";
+        }
+        cout<<endl;
+    }
+    cout<<endl;
+    return 0;
+}
