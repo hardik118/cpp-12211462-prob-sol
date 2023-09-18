61c6300cc5859c0ca637ae89121fd477ddfdc2bb
hardik yadav <hardikyadav1182005@gmail.com>
2023-09-18 20:53:43 +0530

    enw

---
+++ cpp/concpet of inheritence in cpp.cpp
@@ -0,0 +1,36 @@
+/* when a child calss is derived from parent class the child class has the aceess to all properties to parent class 
+ but that does not mean  the parent class  has acess to child class  properties*/
+ //  inheritence is a process in which an object accquires all the properties and behaviour of its paretn object auotmatiucally 
+ // in such a way , we can reuse ,extend or modify the attributes and bheaviours which are defined in thge other class ;
+
+ 	// advantge reusability 
+ 	// there arew two classe NAMELY  BASE CLASS AND PARENT CLASS 
+ 	#include<iostream>
+ 	using namespace std;
+ 	
+ 	
+ 	
+ 	class animal{
+ 		public :
+ 		void eat(){
+ 			cout<< "dog is eating "<<endl;
+ 			
+		 }
+		 void sleep (){
+		 	cout<<" animla os sleeping "<<endl;
+		 	
+		 }
+ 		
+	 };
+	 class dog  : public animal{
+	 	public:
+	 		void brak(){
+	 			cout<< "dog is braking "<<endl;
+	 			
+			 }
+	 };
+	 int main(){
+	 	dog d1;
+	 	d1.eat();
+	 }
+ 
