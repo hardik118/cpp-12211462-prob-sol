92e8bf3832025900e45b8f47a3092529785b0e8d
hardik yadav <hardikyadav1182005@gmail.com>
2023-09-26 19:46:52 +0530

    New

---
+++ cpp/hierarcial inheritecne.cpp
@@ -0,0 +1,13 @@
+#inlcude<iostream>
+using namespace std;
+// in this  we derive  more than one child class from a base clas 
+class animation{
+	/*running
+	jumping */ 
+};
+class character: public animation{
+	 //swim
+};
+class character2: public animation{
+	//kick
+};
