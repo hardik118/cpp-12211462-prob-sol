d740d21f3bf52020791e2349c0fb420e23d37ddf
hardik yadav <hardikyadav1182005@gmail.com>
2023-10-03 19:57:41 +0530

    ew

---
+++ cpp/understaing memory managemt in cpp using new.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+int getsum(int* arr , int n){
+	int sum=0;
+	for(int i=0;i<n;i++){
+		sum=sum+arr[i];
+	}
+	return sum;
+}
+int main(){
+	int n;
+	cin>>n;
+	// varibale type array
+	int* arr =new int[n];
+	for(int i=0;i<n;i++){
+		cin>>arr[i];
+		
+	}
+	int ans= getsum(arr,n);
+	cout<<"sum is :"<<ans<<endl;
+	
+	
+}
