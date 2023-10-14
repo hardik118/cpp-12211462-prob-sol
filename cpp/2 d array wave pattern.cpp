9a3482e5399d4b9dc8712dffb488f03828f3639e
hardik yadav <hardikyadav1182005@gmail.com>
2023-10-14 23:34:11 +0530

    EWN

--- cpp/2 d array wave pattern.cpp
+++ cpp/2 d array wave pattern.cpp
@@ -4,11 +4,11 @@ int main() {
     int matrix[3][3] = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
     
     for (int i = 0; i < 3; i++) {
-        if (i % 2 == 0) { // Even rows
+        if (i % 2 == 0) {
             for (int j = 0; j < 3; j++) {
                 printf("%d ", matrix[i][j]);
             }
-        } else { // Odd rows
+        } else { 
             for (int j = 2; j >= 0; j--) {
                 printf("%d ", matrix[i][j]);
             }
