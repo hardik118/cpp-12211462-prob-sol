4909405af01ad78b491b78c5d7c80cd161afd30a
hardik yadav <hardikyadav1182005@gmail.com>
2023-11-17 23:49:25 +0530

    new

---
+++ cpp/use of struct  in cpp.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+struct emp {
+	int emp_id;
+	string emp_dob;
+	string emp_join;
+	
+};
+int main(){
+	emp emp1;
+cin>>emp1.emp_id;
+cout<<emp1.emp_id;
+cin>>emp1.emp_dob;
+cout<<emp1.emp_dob;
+cin>>emp1.emp_join;
+cout<<emp1.emp_join;
+
+}
