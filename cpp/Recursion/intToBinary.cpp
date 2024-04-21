2ae7dc2dd945643730e8ecdf07cb7f59c44632eb
hardik yadav <hardikyadav1182005@gmail.com>
2024-04-21 23:46:11 +0530

    commit

--- cpp/Recursion/intToBinary.cpp
+++ cpp/Recursion/intToBinary.cpp
@@ -5,7 +5,8 @@ int main(){
     long long int n=9;
     string str=bitset<sizeof(long long)*8>(n).to_string();
     str=str.substr(str.find('1'));
-    
+
     cout<<str;
     return 0;
 }
+/*this is a constant time function*/
