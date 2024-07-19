9bbcb2007b1f67f21573cc85e991cf75cffe8982
hardik yadav <hardikyadav1182005@gmail.com>
2024-07-20 02:07:39 +0530

    commit

---
+++ cpp/Recursion/Revesion/bublesort.cpp
@@ -0,0 +1,30 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+#include<string>
+using namespace std;
+void bublesort(int *arr, int n){
+    if (n==0 || n==1){
+        return ;
+    }
+    for( int i=0; i< n;i++){
+        if(arr[i] > arr[i+1] ){
+            swap(arr[i], arr[i+1]);
+            
+        }
+        
+    }
+    bublesort(arr, n-1);
+    
+    
+}
+int main() {
+int arr[6]={2, 90, 45, 0, 1,5};
+ bublesort(arr, 5);
+ for( int i :arr){
+     cout<<i<<" "<<endl;
+     
+ }
+ 
+
+    return 0;
+}
